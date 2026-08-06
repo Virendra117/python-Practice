5ca4a44a1890c6c9a836dbe245272ce05e86535f
Virendra117 <v3itechnology@gmail.com>
2026-08-06 07:20:11 +0000

    682026

--- ch4.py
+++ ch4.py
@@ -1,5 +1,29 @@
 # Array in Python
 
+# Summary Table
+# Function	Work
+# append(x)	End mein element add karta hai
+# insert(i, x)	Index par element add karta hai
+# remove(x)	Value delete karta hai
+# pop(i)	Index se element delete karta hai
+# index(x)	Element ka index batata hai
+# count(x)	Value kitni baar hai
+# reverse()	Array ko reverse karta hai
+# extend(arr)	Do arrays ko jodta hai
+# buffer_info()	Memory information deta hai
+# typecode	Data type batata hai
+# len(arr)	Total elements batata hai
+# Important Type Codes
+# Type Code	Data Type
+# 'i'	Integer
+# 'f'	Float
+# 'd'	Double
+# 'u'	Unicode Character
+
+
+
+
+
 # Decleration of Array
 
 arr=[1,2,3,4,5,6,7,8,9] 
@@ -22,3 +46,26 @@ for item in fruits:
     print(item)
 
 
+
+# append(x)	End mein element add karta hai
+
+students=["Virendra","Ritesh","Karan","Prakash"]
+
+students.append("Anuj")
+print(students)
+
+
+# insert(index, Value)	Index par element add karta hai
+
+students.insert(2,'Mahek')  
+print(students)
+
+
+
+# remove(x)	Value delete karta hai
+students.remove('Mahek')
+print(students)
+
+
+# pop(i)	Index se element delete karta hai
+students.pop()
