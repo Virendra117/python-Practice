de25134c7a3f512dbda936af01f1ce0840303888
Virendra117 <v3itechnology@gmail.com>
2026-08-12 17:57:17 +0000

    343

---
+++ function.py
@@ -0,0 +1,17 @@
+# Functions
+# -------------------------------
+# Defining functions
+# Calling functions
+# Parameters
+# Arguments
+# Default arguments
+# Keyword arguments
+# Positional arguments
+# *args
+# **kwargs
+# Return statement
+# Multiple return values
+# Scope
+# Local/global variables
+# Lambda functions
+# Recursive functions
