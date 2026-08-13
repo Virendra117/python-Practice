3c431f21c73f287933af22741025c15ddb486727
Virendra117 <v3itechnology@gmail.com>
2026-08-13 07:48:32 +0000

    45454

--- function.py
+++ function.py
@@ -49,3 +49,43 @@ def summarize(*args, **kwargs):
     print("Keyword:", kwargs)
 
 summarize(1, 2, 3, status="active", role="admin")
+
+
+
+def add_numbers(*args):
+    # 'args' is treated as a tuple inside the function
+    return sum(args)
+
+print(add_numbers(1, 2))          # Output: 3
+print(add_numbers(10, 20, 30, 40)) # Output: 100
+
+
+
+# **kwargs (Keyword Arguments)
+# The ** operator unpacks incoming named/keyword arguments into a dictionary.
+# Use case: When you want to accept flexible, named parameters (like configuration settings or metadata).
+
+
+def print_user_profile(**kwargs):
+    # 'kwargs' is treated as a dictionary inside the function
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+print_user_profile(name="Alex", role="Developer", status="Active")
+# Output:
+# name: Alex
+# role: Developer
+# status: Active
+
+
+# Combining args and kwargsYou can use both in the same function definition. 
+# When combining parameter types, 
+# Python requires a strict order:
+# Standard Parameters $\rightarrow$ *args $\rightarrow$ Default Parameters $\rightarrow$ **kwargs
+
+def log_event(event_type, *args, level="INFO", **kwargs):
+    print(f"[{level}] Event: {event_type}")
+    print("Positional details:", args)
+    print("Metadata:", kwargs)
+
+log_event("USER_LOGIN", "User_123", "IP_192.168.1.1", level="DEBUG", status="Success")
